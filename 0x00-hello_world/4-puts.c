904f903e45c810e526e83bf114ac9efad356e1bb
root <root@DESKTOP-7GKI2NG.localdomain>
2021-08-29 18:42:41 +0100

    made changes to support betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 	{
-	 ("\"Programming is like building a multilingual puzzle");
+	put("\"Programming is like building a multilingual puzzle");
 		return(0);
 	}
 	
