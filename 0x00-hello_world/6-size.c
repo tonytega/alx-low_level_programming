6f60d720037618413b3533a06d9a9e01827772a0
root <root@DESKTOP-7GKI2NG.localdomain>
2021-08-29 22:44:53 +0100

    ensured it followed betty coding style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	printf("Size of a char: %c byte(s)\n", sizeof(char));
 	printf("Size of an int: %c byte(s)\n", sizeof(int));
-	printf("Size of a long int: %c byte(s)\n",sizeof(long int));
+	printf("Size of a long int: %c byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %c byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %c byte(s)\n", sizeof(float));
 	return (0);
