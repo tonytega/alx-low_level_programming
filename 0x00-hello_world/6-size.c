3a397cee7ec787d2da3b1060d40ccbf632a6fc62
root <root@DESKTOP-7GKI2NG.localdomain>
2021-08-30 11:35:53 +0100

    changed the %c to %lu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - Print size of differnt data types
  * Description: This prints the size of various data types e.g char
  * Return: 0 show it executed without error
@@ -8,8 +8,8 @@ int main(void)
 {
 	printf("Size of a char: %c byte(s)\n", sizeof(char));
 	printf("Size of an int: %c byte(s)\n", sizeof(int));
-	printf("Size of a long int: %c byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %c byte(s)\n", sizeof(long long int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %c byte(s)\n", sizeof(float));
 	return (0);
 }
