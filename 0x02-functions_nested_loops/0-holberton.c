34903580a1823c135d2cc2063a168bf46a0908b3
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-01 10:21:13 +0100

    Corrected the pointer

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,11 @@
 # include <unistd.h>
-int _putchar(char c);
+int _putchar(char *c);
 int main(void)
 {
-	char c = "_putchar";
-	putchar("%d\n", c);
+	_putchar("holberton");
 	return (0);
 }
-int _putchar(char c)
+int _putchar(char *c)
 {
 	return (write(1, &c, 1));
 }
