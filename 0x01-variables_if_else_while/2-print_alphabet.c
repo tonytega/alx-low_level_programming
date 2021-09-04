7dbdd44b1cd322cc53fe231e064070ae6b9bf696
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-04 20:48:33 +0100

    print leters of the alphabet in small letters using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- entry point of program
+ * Description: This program prints the alphabet in
+ * lower case using lowercase
+ * Return: 0 if successfull
+ **/
+int main(void)
+{
+	char n = "abcdefghijklmnopqrstuvwxyz";
+	putchar("%c\n", n );
+	return (0);
+}
