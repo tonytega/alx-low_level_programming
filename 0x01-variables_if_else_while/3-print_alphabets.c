057c8624831d039efef027f6ce77fc98b4a5cad3
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-14 17:18:17 +0100

    made sure code complies with betty coding style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,14 +10,13 @@ int main(void)
 	 char i;
 	 char j;
 
-        for (i = 'a'; i <= 'z' ; i++)
+	for (i = 'a'; i <= 'z' ; i++)
 	{
-              putchar(i);
+		putchar(i);
 	}
-
 	for (j = 'A'; j <= 'Z' ; j++)
 	{
-             putchar(j);
+		putchar(j);
 	}
 	putchar('\n');
 	return (0);
