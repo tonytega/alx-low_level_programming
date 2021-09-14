eeb93903ccd11cb6e09ba264438e6ca2077eb94f
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-15 00:05:32 +0100

    print all single digit of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+/**
+ * main - entry point of code
+ * Description: Print all single digit of base 10
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
