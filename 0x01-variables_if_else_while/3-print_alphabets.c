b8e05368ec45dfb8bffc2a3a1ea272959c648077
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-14 17:10:44 +0100

    print alphabet in lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main- entry point of program
+ * Description: This program prints the alphabet in
+ * lower case and upper casee using putchar
+ * Return: 0 if successfull
+ **/
+int main(void)
+{
+	 char i;
+	 char j;
+
+        for (i = 'a'; i <= 'z' ; i++)
+	{
+              putchar(i);
+	}
+
+	for (j = 'A'; j <= 'Z' ; j++)
+	{
+             putchar(j);
+	}
+	for 
+	      putchar('\n')
+	return (0);
+}
