abd50042ed94f09996046023f2f12307de47c848
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-15 10:14:35 +0100

    print all numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+/**
+ * main - entry point of code
+ * Description: prints all the numbers of
+ * base16 in lowercase
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i = 1;
+	char j;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+
+	for (j = 'a'; j <= 'f';)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+}
