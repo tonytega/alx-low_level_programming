732136aea7d117b262a3fb49cb02d796bf96e926
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-16 12:42:21 +0100

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+int main(void)
+{
+	putchar('-');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
