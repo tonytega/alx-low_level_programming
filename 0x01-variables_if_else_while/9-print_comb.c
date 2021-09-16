7de648c350a06117ce06bd59fddd1687daf78041
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-16 11:40:58 +0100

    Print all sigle digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point of code
+ * Description: Print all single-digit
+ * numbers followed by a , and a space
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
