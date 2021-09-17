2cdb4b81d51e67c71ddbdf029acff31013ecde78
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-17 15:58:48 +0100

    A functionn that prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point of code
+ * Description: this is a function that prints
+ * the alphabet
+ * Return: returns #include <unistd.h>(0)
+ */
+
+int main(void)
+{
+       for (c = 'a'; c <= 'z'; c++)
+       {
+	_putchar(c);
+	}
+	_putchar('\n');
+ 	return (0);	
+
+}
+
