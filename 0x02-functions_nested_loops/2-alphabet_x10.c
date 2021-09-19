f8f396fc511f0bb43b0a1e5e233c4dec66a70acd
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-19 01:14:47 +0100

    print the alphabet ten times

---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ * Description: This is a function that
+ * prints the alphabet ren times
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 1;
+	char c;
+
+	while (1 < 11)
+	{
+		for (c = 'a' ; c <= 10 ; c++)
+		{
+			putchar(c);
+		}
+		putchar('\n');
+	}
+}
