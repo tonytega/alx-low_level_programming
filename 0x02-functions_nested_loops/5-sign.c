861ed7c3ac9d8ca19390c012c116ebf3cf414061
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-20 13:48:59 +0100

    made changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,14 +14,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1, putchar('+'));
+	putchar('+');
+	return (1);
 	}
 	else if (n == 0)
 	{
-		return (0, putchar('0'));
+		putchar('0');
+		return (0);
 	}
 	else
 	{
-		return (-1, putchar('-'));
+		putchar('-');
+		return (-1)
 	}
 }
