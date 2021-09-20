c1c0e567469f01f8b5c392f6455871e30499766e
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-20 10:40:27 +0100

    check if a parrameter is alphabetic

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ *_isalpha - checks for lowercase
+ * Description: checks for alphabetic characters
+ * @c: This is the parameter to be checked
+ * Return: returns 1 if its a letter,lowercase
+ * or uppercase and returns 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);					}
+	else
+	{
+		return (0);				}
+}
