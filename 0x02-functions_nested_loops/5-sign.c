a344a32164e4521d7f76e720a27012b614cc1da8
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-20 11:31:00 +0100

    checks for the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * print_sign - print the sign of a number
+ * @n: parameter to be checked
+ *
+ * Description: tells you the sign of number
+ * return: returns 1 if positive, 0 if zero
+ * and -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar(0);
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
