c31eec2faaa192b3a78dfeaf760e3d1cd53f848c
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-21 17:39:46 +0100

    computes the last digit of a digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - print last digit of an
+ * integer
+ * Description: A function that returns the last
+ * digit of a number
+ * @c: parsmeter to be computed
+ * Return: return an integer
+ */
+
+int print_last_digit(int c)
+{
+	return ((c % 10));
+}
