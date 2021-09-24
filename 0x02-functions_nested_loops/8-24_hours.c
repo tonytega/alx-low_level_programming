435004d71a8ab42054b83ba533c8bc523f489756
root <root@DESKTOP-7GKI2NG.localdomain>
2021-09-24 22:51:09 +0100

    tummy advive

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+void jack_bauer(void);
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	while (a <= 2)
+	{
+		while (b <= 3)
+		{
+			while (c <= 5)
+			{
+				while (d <= 9)
+				{
+					putchar('a');
+					putchar('b');
+					putchar(':');
+					putchar('c');
+					putchar('d');
+					putchar('\n');
+					d++;		
+				}
+				putchar('a');
+				putchar('b');
+				putchar('c');
+				putchar('d');
+				putchar('\n');
+				c++;
+			}
+			putchar('a');
+			putchar('b');
+			putchar('c');
+			putchar('d');
+			b++;
+		}
+		putchar('a');
+		putchar('b');
+		putchar('c');
+		putchar('d');
+		a++;
+	}
+}
+
