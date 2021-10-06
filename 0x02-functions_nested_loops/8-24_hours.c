c839893652a2479706bc1cd4bad33264d2c2884b
root <root@DESKTOP-7GKI2NG.localdomain>
2021-10-06 12:42:04 +0100

    made changes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,43 +7,17 @@ void jack_bauer(void)
 	char b ='0';
 	char c = '0';
 	char d = '0';
-
-	while (a <= '2')
+	while ( d <= 9)
 	{
-		while (b <= '3')
-		{
-			while (c <= '5')
-			{
-				while (d <= '9')
-				{
-					putchar(a);
-					putchar(b);
-					putchar(':');
-					putchar(c);
-					putchar(d);
-					putchar('\n');
-					d++;		
-				}
-				putchar(a);
-				putchar(b);
-				putchar(':');
-				putchar(c);
-				putchar(d);
-				putchar('\n');
-				c++;
-			}
-			putchar(a);
-			putchar(b);
-			putchar(':');
-			putchar(c);
-			putchar(d);
-			b++;
-		}
 		putchar(a);
 		putchar(b);
 		putchar(':');
 		putchar(c);
 		putchar(d);
-		a++;
-	}
+		putchar('\n');
+		d++;
+
 }
+
+
+	
