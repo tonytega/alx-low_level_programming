657f61d68e11bd1c219555a63e3b0a5881f7feb5
anthony <anthony@ubuntu-22.04.2.myguest.virtualbox.org>
2023-07-27 09:27:35 +0100

    made code conform to betty documentation style

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Description: a program that prints all possible different combinations of two digits.
+ * @parameter: takes nothing
+ * 
+ * Return: 0 always
+ */
 int main(void)
 {
 	int i = 0;
 	int j = 1;
 
-	while ( i < 10)
+	while (i < 10)
 	{
-	
 		while (j < 10)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
 
-			if( i < 8 || j < 9 )
+			if (i < 8 || j < 9)
 			{
 			putchar(',');
 			putchar(' ');
 			}
 			j++;
 		}
-		
 	i++;
-	j = 1 + i ;
+	j = 1 + i;
 	}
 	putchar('\n');
-	return(0);	
+	return (0);
 }
