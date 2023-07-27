0e5ae472cee9b56fd1660f67de54a84f7b7942e5
anthony <anthony@ubuntu-22.04.2.myguest.virtualbox.org>
2023-07-27 23:04:17 +0100

    trying to commit to betty coding style

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,24 +6,28 @@
  * Description: prints all possible different combination of 3 numbers
  * Return: 0 always
  */
-int main() {
-    int i, j, k;
+int main(void)
+{
+	int i, j, k;
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 
-    for (i = 0; i <= 7; i++) {
-        for (j = i + 1; j <= 8; j++) {
-            for (k = j + 1; k <= 9; k++) {
-                putchar(i + '0');
-                putchar(j + '0');
-                putchar(k + '0');
+                		if (i < 7 || j < 8 || k < 9)
+				{
+                    			putchar(',');
+                    			putchar(' ');
+				}
+			}
+		}
+	}
 
-                if (i < 7 || j < 8 || k < 9) {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-
-    putchar('\n');
-    return 0;
+	putchar('\n');
+	return 0;
 }
