d1634421662177b5744be858c5c3e38fc4564232
anthony <anthony@ubuntu-22.04.2.myguest.virtualbox.org>
2023-07-28 00:12:30 +0100

    trying to commit to betty coding style

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	int i, j, k;
 	for (i = 0; i <= 7; i++)
+
 	{
 		for (j = i + 1; j <= 8; j++)
 		{
@@ -19,15 +20,15 @@ int main(void)
 				putchar(j + '0');
 				putchar(k + '0');
 
-                		if (i < 7 || j < 8 || k < 9)
+				if (i < 7 || j < 8 || k < 9)
 				{
-                    			putchar(',');
-                    			putchar(' ');
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
 
 	putchar('\n');
-	return 0;
+	return (0);
 }
