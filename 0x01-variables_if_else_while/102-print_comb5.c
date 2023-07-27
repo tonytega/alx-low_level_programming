fba4b857a9901c7b70451e90a53c0498611b1438
anthony <anthony@ubuntu-22.04.2.myguest.virtualbox.org>
2023-07-28 00:24:12 +0100

    combines two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i, j;
+
+    for (i = 0; i <= 99; i++)
+    {
+        for (j = i; j <= 99; j++)
+        {
+            if (i != j)
+            {
+                // Print the first two-digit number
+                putchar((i / 10) + '0');
+                putchar((i % 10) + '0');
+
+                putchar(' ');
+
+                // Print the second two-digit number
+                putchar((j / 10) + '0');
+                putchar((j % 10) + '0');
+
+                if (i < 98 || j < 99)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+
+    putchar('\n');
+    return (0);
+}
